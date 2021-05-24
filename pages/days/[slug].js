b5d6e900ec43f8699a722fcb5849a344e5017ac1
Graham <gtwright@gmail.com>
2021-05-24 10:39:32 -0400

    fallback on slug

--- pages/days/[slug].js
+++ pages/days/[slug].js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { initializeApollo } from "../../lib/apolloClient";
 import ReactPlayer from "react-player";
 import moment from "moment";
+import Link from "next/link";
 
 const DAY_QUERY = gql`
   query homePage($slug: String) {
@@ -21,23 +22,30 @@ const DAY_QUERY = gql`
   }
 `;
 
-const SessionPage = ({ day, slug }) => {
+const DayPage = ({ day, slug }) => {
   const { loading, error, data } = useQuery(DAY_QUERY, {
-    slug
+    slug,
   });
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   // session = data ? data.sessionCollection.items[0] : session;
   var today = moment();
   var release = moment(day?.releaseDatetime) || null;
   const released = release && today >= release;
-  console.log(day);
+  // console.log(day);
   return (
     <Layout>
       <Paper style={{ padding: 30 }}>
         {day ? (
           <>
+            <div>
+              <Link href="/">&larr; Back to Home</Link>
+            </div>
             <h1>{day.title}</h1>
             <div style={{ marginTop: 30, marginBottom: 30 }}>
-              {documentToReactComponents(day.description)}
+              {/* {documentToReactComponents(day.description)} */}
             </div>
             {released ? (
               <>Released on {release.format("L")}</>
@@ -45,8 +53,6 @@ const SessionPage = ({ day, slug }) => {
               <>Releasing on {release.format("L")}</>
             )}
           </>
-        ) : loading ? (
-          <div>Loading...</div>
         ) : (
           <div>Page Not Found</div>
         )}
@@ -55,31 +61,35 @@ const SessionPage = ({ day, slug }) => {
   );
 };
 
-export async function getStaticProps({ params, preview = false }) {
+export async function getStaticProps({ params }) {
   const apolloClient = initializeApollo();
   const { slug } = params;
   const client = await apolloClient.query({
     query: DAY_QUERY,
-    variables: { slug }
+    variables: { slug },
   });
   const day = client.data.dayCollection.items[0] || null;
-  // console.log("session", slug, session);
+  console.log("day", day);
 
   return {
     props: {
       // initialApolloState: apolloClient.cache.extract(),
       day,
-      slug
+      slug,
     },
-    revalidate: 1
+    revalidate: 1,
+    // notFound: false,
   };
 }
 
 export async function getStaticPaths() {
+  const paths = [...Array(10).keys()].map((x) => ({
+    params: { slug: `${x + 1}` },
+  }));
   return {
-    paths: [],
-    fallback: true // See the "fallback" section below
+    paths,
+    fallback: true,
   };
 }
 
-export default SessionPage;
+export default DayPage;
